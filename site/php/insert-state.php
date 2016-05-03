307a82b3f98089d2ad4855b24ef3278eeb494172
shawn.hillyer@gmail.com <shawn.hillyer@gmail.com>
2016-05-03 16:52:53 -0700

    Fix state regex and remove strlen check because redundant

--- site/php/insert-state.php
+++ site/php/insert-state.php
@@ -4,31 +4,30 @@
 // $state_name = addslashes(trim($_POST['input_state_name']));
 
 // Extract the post variables and do some basic formatting
-$state_name = trim($_POST['input_state_name']);
-$state_abbrev = strtoupper(trim($_POST['input_state_abbr']));
+$state_name = trim($_POST['input_state_name']);  // trim whitespace
+$state_abbrev = strtoupper(trim($_POST['input_state_abbr'])); // trim whitespace and convert to uppercase
 
+// DEBUG ECHO
 echo '<p>Hello from insert-state.php</p><ul>'
 foreach ($_POST as $input) {
 	echo '<li>$input: '.$input.'</li>';
 }
 echo '</ul>'
+// END DEBUG ECHO
 
-// Constants to help clarify the validation calls
-define('ABBREV_LENGTH', 2);
 
 // Validate that abbreviation is exactly two characters long
-if (strlen($state_abbrev) != ABBREV_LENGTH) {
-	echo '<p>Abbreviation must be exacty '.ABBREV_LENGTH.' characters long.<p>';
-	exit;
+if (!ereg('^[[:upper:]][[:upper]]$', $state_abbrev)) {
+	echo '<p>Abbreviation must be exactly '.ABBREV_LENGTH.' letters (No numbers, spaces, or special characters allowed).<p>';
 }
-// Compare $state_abbrev to see if matches regix patter of two [[:upper:]]. 
-// TODO FIX THIS NEXTS LINE GOT INTERUPPTED:
-if ($state_abbrev  ^[[:upper:]][[:upper]]$ )
 
-// Attempt to insert new state
+else {
+	// Attempt to insert new state
 
-//INSERT INTO state(name, abbreviation)
-//    VALUES([$state_name], [$state_abbrev]);
+	//INSERT INTO state(name, abbreviation)
+	//    VALUES([$state_name], [$state_abbrev]);
+}
+
 
 
 
