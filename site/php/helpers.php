2ba48efc26d0536ad64414506679b4842e9939af
shawn.hillyer@gmail.com <shawn.hillyer@gmail.com>
2016-05-03 17:26:33 -0700

    Update helpers.php

--- site/php/helpers.php
+++ site/php/helpers.php
@@ -3,8 +3,13 @@
 	// Include any external libraries here using require_once('file.php')
 	// (The idea is we would use this as a single master header file and include only the one we need across all our files)
 
-		
-	echo '<p><a href="../index.html">Back to Data Entry Page</a></p>';
+
+// @param {string} url : Link for href attribute
+// @param {string} lable : Text on the button
+// @param {sring} style : suffix for the bootstrap btn- styles: default | primary | success |  info | warning | etc.
+function insert_button($url, $label, $style='default') {
+	echo '<p><a href="'.$url.'" class="'.$style.'">'.$description.'</a></p>'
+}		
 
 
 
