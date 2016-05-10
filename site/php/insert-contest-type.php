681685a2e8faf4ada1577512d96ad0f11752df3a
shawn.hillyer@gmail.com <shawn.hillyer@gmail.com>
2016-05-10 12:26:02 -0700

    Update insert-contest-type.php

--- site/php/insert-contest-type.php
+++ site/php/insert-contest-type.php
@@ -3,26 +3,23 @@ ini_set('display_errors', 'On');
 require_once( 'helpers.php' );
 require_once("php/Page.php");
 
-
 $page = new Page();
 $page->header = 'Insert Candidate into Database';
 
-// If embedding any 'quotes' then \'escape them\' !!!!
-$page->content = ''; // I think need to embed everything from here to END_EMBED comment
+// Use HEREDOC to assign the php for this particular page to the page's content variable
+// $page->content = <<<EOCONTENT // TODO: Uncomment this after debugging page (also its matching end market near need)
 
-// Extract the post variables
+// Extract POST variables
 $contest_type = trim($_POST['input_contest_type']);
 
-echo '<p>Hello from insert-contest-type.php</p><ul>';
-foreach ($_POST as $input) {
-	echo '<li>$input: '.$input.'</li>';
-}
-echo '</ul>';
-
-// Validate the input (make sure it's at least, say, 3 characters for first and last name)
-if (false) {
-	// change false to whatever validation we wanted to look for
-}
+// Data validation
+define('CTYPE_MIN', 3);
+$contest_type_is_valid = hasLengthInRange($contest_type, CTYPE_MIN, 255);
+if (!$contest_type_is_valid) {
+	echo '<p>Contest type must be at least '.CTYPE_MIN.' letters long.</p>';
+	insert_button("../index.php", "Back");
+	exit;
+}	
 
 else {
 	// Add slashes if needed
@@ -30,35 +27,32 @@ else {
 		$contest_type = addslashes($contest_type);
 	}
 
-	// the @ sign is the error suppressino operator, so we can gracefully handle exceptions
-	@ $db = new mysqli('serverhost', 'username', 'password', 'db-name');
-	
-	if (mysqli_connect_errno()) {
-		echo '<p>Error: Could not connect to the database. Please try again later.</p>';
-		// TODO: probably print a button here to go back to insert page then exit
-		insert_button("../index.php", "Back");
+	// connect to DB -- returns null on failure so we exit
+	if(($db = connectToDb()) == null) {
 		exit;
 	}
-
+	
+	// Preload query then fill in the user input (prevents SQL Injection attack)
 	$query = 'INSERT INTO contest_type(name) VALUES(?)';
 	$stmt = $db->prepare($query);
 	$stmt->bind_param('s', $contest_type);
 	$stmt->execute();
 
-	// Process resuls shere
-	echo '<p>'.$db->affected_rows.' candidate added to database.</p>';
-	$stmt->close(); // Might be able to move this to right after the ->execute() call??
+	// Process results
+	if ($db->affected_rows >= 0) {
+		echo '<p>'.$db->affected_rows.' contest type added to database.</p>';
+	}
+	else {
+		echo '<p>Unable to add contest type to database - probably already exists.</p>';
+	}
 	
 	// Close resources and close connection
-	$result->free();
-	$db->close();	
+	$stmt->close(); // Might be able to move this to right after the ->execute() call??
+	$db->close();
 }
 
-$page->Display();
-
-
-// Attempt to insert the new contest_type
-//INSERT INTO contest_type(name) VALUES([$contest_type]);
-
+insert_button("../index.php", "Back");
+// EOCONTENT; // TODO: Uncomment this line + the next line once page debugged (and matching heredoc near top)
+// $page->Display();
 
 ?>
