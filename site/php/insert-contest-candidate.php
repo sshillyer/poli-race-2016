f1b5b7c7c4b5cec6752d6639750c41169af8ac14
shawn.hillyer@gmail.com <shawn.hillyer@gmail.com>
2016-05-10 12:41:47 -0700

    Revise insert-contest-candidate.php

--- site/php/insert-contest-candidate.php
+++ site/php/insert-contest-candidate.php
@@ -3,7 +3,6 @@ ini_set('display_errors', 'On');
 require_once( 'helpers.php' );
 require_once("php/Page.php");
 
-
 $page = new Page();
 $page->header = 'Insert Candidate-Contest Data into Database';
 
@@ -30,15 +29,20 @@ foreach ($_POST as $input) {
 echo '</ul>'
 ////////////////////// DEBUG ECHO
 
+
 // Data Validation
+
 // We aren't going to validate much here. A contest-candidate record can
 // attempt to be inserted with bad data, but if the firstname, lastname, state, party name are not
 // found then the database will just return null records for those fields and the insert will fail
 // because we have "NOT NULL" in our table definition. We also allow
 // NULL inserts for the vote_count and delegate_count. We do need to validate
 // that the number of votes is non-negative, though!
-if ($contest_votes < 0 || $contest_delegates < 0) {
-	echo '<p>Cannot have negative votes or delegates. Try again.</p>';
+
+$only_non_neg_values = ($contest_votes >= 0 && $contest_delegates && 0)
+
+if (!$only_non_neg_values) {
+	echo '<p>Cannot have negative votes or delegates.</p>';
 	exit;	
 }
 
@@ -58,34 +62,26 @@ else {
 	if(($db = connectToDb()) == null)
 		exit;
 
-	// INSERT INTO candidate(fname, lname, party_id)  
-	// VALUES([$candidate_fname], [$candidate_lname], (SELECT p.id FROM party AS p WHERE p.name=[$candidate_party])
+	// Preload query then fill in the user input (prevents SQL Injection attack)
 	$query = 'INSERT INTO contest_candidate(candidate_id, contest_id, vote_count, delegate_count) VALUES((SELECT id FROM candidate WHERE candidate.fname=? AND candidate.lname=?), (SELECT id FROM contest WHERE contest.state_id=(SELECT id FROM state WHERE state.name=?) AND contest.party_id=(SELECT id FROM party WHERE party.name=?)), ?, ?)';
 	$stmt = $db->prepare($query);
 	$stmt->bind_param('ssssii', $candidate_fname, $candidate_lname, $contest_state, $contest_party, $contest_votes, $contest_delegates);
 	$stmt->execute();
 
-	// Process results here
-	echo '<p>'.$db->affected_rows.' candidate added to database.</p>';
-	$stmt->close(); // Might be able to move this to right after the ->execute() call??
+	// Process results
+	if ($db->affected_rows >= 0) {
+		echo '<p>'.$db->affected_rows.' record of contest-candidate details added to database.</p>';
+	}
+	else {
+		echo '<p>Unable to add contest-candidate details to database.</p>';
+	}
 	
 	// Close resources and close connection
-	$result->free();
-	$db->close();	
+	$stmt->close(); // Might be able to move this to right after the ->execute() call??
+	$db->close();
 }
+
+insert_button("../index.php", "Back");
 // EOCONTENT; // TODO: Uncomment this line + the next line once page debugged (and matching heredoc near top)
 // $page->Display();
-
-// Query used:
-//INSERT INTO contest_candidate(candidate_id, contest_id, vote_count, delegate_count)
-//	VALUES
-//	(	(SELECT id FROM candidate WHERE candidate.fname=[$candidate_fname] AND candidate.lname=[$candidate_lname]),
-//		(SELECT id FROM contest 
-//			WHERE contest.state_id=(SELECT id FROM state WHERE state.name=[$contest_state])
-//				AND 
-//			contest.party_id=(SELECT id FROM party WHERE party.name=[$contest_party])
-//		),
-//		[$contest_votes],
-//		[$contest_delegates]);
-
 ?>
