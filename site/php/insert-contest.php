344e20486694153bab3828069746077d9572bde3
Shawn S Hillyer <shawn.hillyer@gmail.com>
2016-05-10 22:53:19 -0700

    Finish and fix insert-contest.php

--- site/php/insert-contest.php
+++ site/php/insert-contest.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 'On');
-require_once( 'helpers.php' );
-require_once("Page.php");
+require_once('helpers.php');
+require_once('Page.php');
 
 // Create new Page object and display top of page content
 $page = new Page();
@@ -19,6 +19,7 @@ $contest_type = trim($_POST['input_contest_contest_type']);
 // using a select statement in the main form page so don't have to validate.
 // Worst case scenario if we don't do that is the insertion fails.
 $dateIsValid = true;
+
 if(!$dateIsValid) {
 	echo '<p>Date is not formatted correctly.</p>';
 	insert_button("../index.php", "Back");
@@ -43,7 +44,7 @@ else {
 	// Preload query then fill in the user input (prevents SQL Injection attack)
 	$query = 'INSERT INTO contest(contest_date, state_id, party_id, contest_type_id) VALUES (?, (SELECT id FROM state WHERE state.name=?), (SELECT id FROM party WHERE party.name=?), (SELECT id FROM contest_type WHERE contest_type.name=?))';
 	$stmt = $db->prepare($query);
-	$stmt->bind_param('ss', $contest_date, $contest_state, $contest_party, $contest_type);
+	$stmt->bind_param('ssss', $contest_date, $contest_state, $contest_party, $contest_type);
 	$stmt->execute();
 
 	// Process results
@@ -51,7 +52,7 @@ else {
 		echo '<p>'.$db->affected_rows.' contest added to database.</p>';	
 	}
 	else {
-		echo '<p>Unable to add state to database - probably already exists.</p>';
+		echo '<p>Unable to add contest to database - probably already exists.</p>';
 	}
 	
 	// Close resources and close connection
@@ -60,7 +61,6 @@ else {
 }
 
 insert_button("../index.php", "Back");
-// EOCONTENT; // TODO: Uncomment this line + the next line once page debugged (and matching heredoc near top)
-// $page->Display();
+$page->DisplayBottom();
 
 ?>
